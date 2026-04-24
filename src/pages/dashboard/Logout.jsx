0f789163cb750c63d3c555a1f00903dc4a1b44b6
Juliana <julianajj0214@gmail.com>
2026-04-24 14:28:44 -0500

    agrego botones para iniciar sesión e ir al dashboard en logout

--- src/pages/dashboard/Logout.jsx
+++ src/pages/dashboard/Logout.jsx
@@ -26,3 +26,28 @@ export const Logout = () => {
         <p className="text-gray-700 text-lg">
           Has cerrado sesión del Sistema de Notas del CESDE.
         </p>
+        <div className="flex justify-center items-center">
+          <button 
+            type="button" 
+            onClick={handleLogin}
+            className="px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md
+            hover:bg-blue-700 hover:shadow-lg
+            focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-1
+            transition duration-300 ease-in-out">
+                Iniciar sesion
+          </button>
+
+          <button 
+            type="button" 
+            onClick={handleDashboard}
+            className="px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md
+            hover:bg-blue-700 hover:shadow-lg
+            focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-1
+            transition duration-300 ease-in-out">
+               Ir al dashboard
+          </button>
+        </div>
+      </section>
+    </main>
+  )
+}
