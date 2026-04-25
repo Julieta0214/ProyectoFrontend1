3fae95fc30d43bbc9c38a569d5083c2cf97bc983
jfuz91 <felipeuz.07@gmail.com>
2026-04-24 20:03:13 -0500

    feat: agregar imports y manejo de estado en FormAdministrador

--- src/pages/completar/FormAdministrador.jsx
+++ src/pages/completar/FormAdministrador.jsx
@@ -1,9 +1,15 @@
-const FormAdministrador = () => {
+import { useState } from "react";
+
+export const FormAdministrador = () => {
+  const [usuario, setUsuario] = useState(null);
+  const [tituloProfesional, setTituloProfesional] = useState("");
+  const [especializacion, setEspecializacion] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
       <h1>FormAdministrador</h1>
     </div>
   );
 };
-
-export default FormAdministrador;
